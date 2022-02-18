f520c855f79042639abb0a6bf81258af4e13ba0e
sonali sonawane <sonawanesonali41@gmail.com>
2022-02-18 18:25:08 +0530

    created folder structure

---
+++ project front end/fullstackproject/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/top-header/header/header.component';
+import { DashboardComponent } from './main-project/dashboard/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
